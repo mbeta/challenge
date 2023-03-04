39e61973cbb380a17ba483cefd14b46d792c75ec
mbetania <martinezbetania@gmail.com>
2023-03-04 19:01:17 -0300

    Inicializacion proyecto

---
+++ src/test/java/ar/com/challenge/precios/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.challenge.precios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
